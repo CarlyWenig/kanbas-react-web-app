4c56b00615743dbcfa38622349651369bb82c762
CarlyWenig <69254490+CarlyWenig@users.noreply.github.com>
2024-02-14 20:40:03 -0500

    started assignment3

---
+++ src/Kanbas/Dashboard/index.tsx
@@ -0,0 +1,42 @@
+function Dashboard() {
+//     return (
+//         <div className="p-4">
+//         <h1>Dashboard</h1>
+//         <hr />
+//         <h2>Published Courses (7)</h2>
+//         <hr />
+//         <div className="row">
+//           <div className="row row-cols-1 row-cols-md-5 g-4">
+//             <div className="col" style={{width: "300px"}}>
+//               <div className="card d-flex flex-column">
+//                 <img
+//                   src="/images/flowers.jpg"
+//                   className="card-img-top"
+//                   style={{max-Height: "150px"; min-Height: "150px"}}
+//                 />
+//                 <div className="card-body">
+//                   <a
+//                     className="card-title"
+//                     href="/Kanbas/Courses/Home/screen.html"
+//                     style="text-decoration: none; color: navy; font-weight: bold"
+//                   >
+//                     CS1234 React JS</a
+//                   >
+//                   <p className="card-text" style="color: gray; margin-bottom: 1px">
+//                     CS1234.12631.202410
+//                   </p>
+//                   <p className="card-text" style="color: gray; margin-bottom: 4px">
+//                     Spring 2024 Semester Full Term
+//                   </p>
+//                   <Link html="#" className="btn btn-primary"> Go </a>
+//                 </div>
+//               </div>
+//             </div>
+//             </div>
+//             </div>
+//             </div>
+//     )
+
+}
+
+export default Dashboard
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,13 +1,22 @@
-import Nav from "../Nav";
-import { Link } from "react-router-dom";
+import KanbasNavigation from "./Navigation";
+import {Routes, Route} from "react-router";
+
 function Kanbas() {
-  return (
-    <div>
-      {/* <Link to="/Labs/a3">A3</Link> |<Link to="/Kanbas">Kanbas</Link> |
-      <Link to="/hello">Hello</Link> | */}
-      <Nav />
-      <h1>Kanbas</h1>
-    </div>
-  );
-}
-export default Kanbas;
+    return (
+      <div className="d-flex">
+        <div>
+          <h1>Kanbas Navigation</h1>
+        </div>
+        <div style={{ flexGrow: 1 }}>
+            <Routes>
+                <Route path="Account" element={<h1>Account</h1>} />
+                <Route path="Dashboard" element={<h1>Dashboard</h1>} />
+                <Route path="Courses" element={<h1>Courses</h1>} />
+            </Routes>
+          <h1>Account</h1>
+          <h1>Dashboard</h1>
+          <h1>Courses</h1>
+        </div>
+      </div>
+  );}
+  export default Kanbas;
