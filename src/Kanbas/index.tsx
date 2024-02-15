bce39265752905d64dee6773e5359ef4a09102ef
CarlyWenig <69254490+CarlyWenig@users.noreply.github.com>
2024-02-14 20:05:18 -0500

    lab

---
+++ src/Kanbas/index.tsx
@@ -0,0 +1,13 @@
+import Nav from "../Nav";
+import { Link } from "react-router-dom";
+function Kanbas() {
+  return (
+    <div>
+      {/* <Link to="/Labs/a3">A3</Link> |<Link to="/Kanbas">Kanbas</Link> |
+      <Link to="/hello">Hello</Link> | */}
+      <Nav />
+      <h1>Kanbas</h1>
+    </div>
+  );
+}
+export default Kanbas;
