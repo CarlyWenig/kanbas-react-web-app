a5e4ad8cf4cf9862d2384f310932e5aaacee6ac4
CarlyWenig <69254490+CarlyWenig@users.noreply.github.com>
2024-03-10 23:54:45 -0400

    modules refractored

--- src/Kanbas/Courses/Modules/List.tsx
+++ src/Kanbas/Courses/Modules/List.tsx
@@ -1,27 +1,27 @@
 import React, { useState } from "react";
-import '../../../../src/Kanbas/Courses/Modules/index.css';
-import { modules } from "../../Database";
-import { FaEllipsisV, FaCheckCircle, FaPlusCircle, FaPlus } from "react-icons/fa";
-import { useParams } from "react-router";
-import "../Modules/index.css";
+import { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { addModule, deleteModule, updateModule, setModule } from "./reducer";
+import { KanbasState } from "../../store";
+import { FaEllipsisV, FaPlus } from "react-icons/fa";
 
 function ModuleList() {
   const { courseId } = useParams();
-  const modulesList = modules.filter((module) => module.course === courseId);
-  const [selectedModule, setSelectedModule] = useState(modulesList[0]);
+  const moduleList = useSelector(
+    (state: KanbasState) => state.modulesReducer.modules
+  );
+  const module = useSelector(
+    (state: KanbasState) => state.modulesReducer.module
+  );
+  const dispatch = useDispatch();
+
   return (
     <>
-       <div className="d-flex justify-content-end modules-container">
-        <button className="custom-button">
-          Collapse All
-        </button>
-        <button className="custom-button">
-          View Progress
-        </button>
+      <div className="d-flex justify-content-end modules-container">
+        <button className="custom-button">Collapse All</button>
+        <button className="custom-button">View Progress</button>
         <select className="custom-button">
-          <option value="Publish All">
-            Publish All
-          </option>
+          <option value="Publish All">Publish All</option>
         </select>
         <button className="mod-button">
           <FaPlus /> Module
@@ -31,38 +31,65 @@ function ModuleList() {
         </button>
       </div>
       <hr />
-
-
-      <ul className="list-group wd-modules">
-        {modulesList.map((module) => (
-          <li
-            className="list-group-item"
-            onClick={() => setSelectedModule(module)}>
-            <div>
-              <FaEllipsisV className="me-2" />
-              {module.name}
-              <span className="float-end">
-                <FaCheckCircle className="text-success" />
-                <FaPlusCircle className="ms-2" />
-                <FaEllipsisV className="ms-2" />
+      <ul className="list-group-item">
+        <li className="list-group-item">
+          <input
+            value={module.name}
+            onChange={(e) =>
+              dispatch(setModule({ ...module, name: e.target.value }))
+            }
+          />
+          <span className="float-end">
+            <button
+              className="custom-button-b"
+              onClick={() => dispatch(updateModule(module))}
+            >
+              Update
+            </button>
+            <button
+              className="custom-button-g"
+              onClick={() =>
+                dispatch(addModule({ ...module, course: courseId }))
+              }
+            >
+              Add
+            </button>
+          </span>
+          <br />
+          <textarea
+            value={module.description}
+            onChange={(e) =>
+              dispatch(setModule({ ...module, description: e.target.value }))
+            }
+          />
+        </li>
+        <hr />
+        {moduleList
+          .filter((module) => module.course === courseId)
+          .map((module, index) => (
+            <li key={index} className="list-group-item">
+              <span style={{ fontSize: "32px", fontWeight: "bold" }}>
+                {module.name}
               </span>
-            </div>
-            {selectedModule._id === module._id && (
-              <ul className="list-group">
-                {module.lessons?.map((lesson) => (
-                  <li className="list-group-item">
-                    <FaEllipsisV className="me-2" />
-                    {lesson.name}
-                    <span className="float-end">
-                      <FaCheckCircle className="text-success" />
-                      <FaEllipsisV className="ms-2" />
-                    </span>
-                  </li>
-                ))}
-              </ul>
-            )}
-          </li>
-        ))}
+              <span className="float-end">
+                <button
+                  className="custom-button-r"
+                  onClick={() => dispatch(deleteModule(module._id))}
+                >
+                  Delete
+                </button>
+                <button
+                  className="custom-button-g"
+                  onClick={() => dispatch(setModule(module))}
+                >
+                  Edit
+                </button>
+              </span>
+              <p>{module.description}</p>
+              <p>{module._id}</p>
+              <hr />
+            </li>
+          ))}
       </ul>
     </>
   );
